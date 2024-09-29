986653afe71f8bf2c48f4d162c96fc70512612e0
javier chung <jchung@sparxworks.com>
2024-09-28 22:07:31 -0500

    first commit

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # Variables
 OPENVPN_CONFIG="/etc/openvpn/client.conf"
 SQUID_CONFIG="/etc/squid/squid.conf"
+SQUID_CACHE_DIR="/var/spool/squid"
 
 # Verificar si los archivos de configuración están disponibles
 if [ ! -f "$OPENVPN_CONFIG" ]; then
@@ -20,6 +21,23 @@ echo "Iniciando OpenVPN..."
 openvpn --config "$OPENVPN_CONFIG" &
 
 # Esperar un momento para asegurarse de que OpenVPN se inicie
+sleep 5
+
+# Crear el directorio de caché de Squid si no existe y dar los permisos adecuados
+echo "Creando el directorio de caché de Squid..."
+mkdir -p $SQUID_CACHE_DIR
+chown -R proxy:proxy $SQUID_CACHE_DIR
+
+# Inicializar el directorio de caché
+echo "Inicializando el directorio de caché de Squid..."
+squid -z -f "$SQUID_CONFIG"
+if [ $? -ne 0 ]; then
+    echo "Error al inicializar el directorio de caché de Squid"
+    cat /var/log/squid/cache.log
+    exit 1
+fi
+
+# Esperar un momento para que se complete la inicialización del caché
 sleep 5
 
 # Iniciar Squid y redirigir los logs a la salida estándar
