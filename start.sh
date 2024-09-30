7c83a5fd1f4b57ca4faffd4d2c29e334b1e4b940
javier chung <jchung@sparxworks.com>
2024-09-30 16:05:31 -0500

    sa

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Iniciar la primera VPN
-openvpn --config /etc/openvpn/vpn1.ovpn --daemon --socks-proxy 127.0.0.1 1080
+openvpn --config /etc/openvpn/vpn1.ovpn --daemon --socks-proxy 0.0.0.0 8080
 
 # Iniciar la segunda VPN
-openvpn --config /etc/openvpn/vpn2.ovpn --daemon --socks-proxy 127.0.0.1 1081
+openvpn --config /etc/openvpn/vpn2.ovpn --daemon --socks-proxy 0.0.0.0 8081
 
 # Mantener el contenedor en ejecución
 tail -f /dev/null
